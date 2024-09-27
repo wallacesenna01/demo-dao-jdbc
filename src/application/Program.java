bbf5bdb2cc446ab99d304d444b4f7425df91d50f
Wallace Artur <wallacearthur_3@hotmail.com>
2024-09-27 20:36:08 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.sql.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -34,6 +35,11 @@ public class Program {
 	    	for(Seller obj : list) {
 			System.out.println(obj);
 		}
+	    	
+	    	  System.out.println("\n=== TEST3: seller findAll ===");	
+	    	  Seller newSeller = new Seller(null,"Greg","Greg@gmail.com", new java.util.Date(), 4000.0, department);
+	    	  sellerDao.insert(newSeller);
+	    	  System.out.println("inserted!  New id = " + newSeller.getId()) ;
 
 	}
 
